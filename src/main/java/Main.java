24c5561594e191f5f34730c750507a28257e62e7
Hugo Lapa <Hugolapa888@gmail.com>
2024-06-02 22:49:26 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,14 +16,29 @@ public class Main {
         final DatagramPacket packet = new DatagramPacket(buf, buf.length);
         serverSocket.receive(packet);
         System.out.println("Received data");
-        
+
         ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-        byteBuffer.putShort((short)1234);
-        byteBuffer.putShort((short)0x8000);
-        byteBuffer.putShort((short)0);
-        byteBuffer.putShort((short)0);
-        byteBuffer.putShort((short)0);
-        byteBuffer.putShort((short)0);
+        
+        // DNS Header
+        byteBuffer.putShort((short)1234); // ID
+        byteBuffer.putShort((short)0x8000); // Flags
+        byteBuffer.putShort((short)1); // QDCount - 1 question
+        byteBuffer.putShort((short)0); // answers
+        byteBuffer.putShort((short)0); // authority records
+        byteBuffer.putShort((short)0); // additional records
+
+
+        // Question Section
+        // Name: codecrafters.io
+        byteBuffer.put((byte) 0x0c); // Length of "codecrafters"
+        byteBuffer.put("codecrafters".getBytes());
+        byteBuffer.put((byte) 0x02); // Length of "io"
+        byteBuffer.put("io".getBytes());
+        byteBuffer.put((byte) 0x00); // Null byte to terminate the name
+
+        // Type and Class
+        byteBuffer.putShort((short) 1); // Type: A (Host Address)
+        byteBuffer.putShort((short) 1); // Class: IN (Internet)
         
         final byte[] bufResponse = byteBuffer.array();
         final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
